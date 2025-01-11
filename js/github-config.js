73d394ad2d7246c5b4299a6cc6a7d5d05fc5f823
Gustav Jes Iversen <gustavjes0105@gmail.com>
2025-01-11 20:11:31 +0100

    Update github-config.js

--- js/github-config.js
+++ js/github-config.js
@@ -1,6 +1,8 @@
 const GITHUB_CONFIG = {
     APP_ID: '1109199',
     CLIENT_ID: 'Iv23liDmxhb0KXNF78gT',
+    INSTALLATION_ID: '59455281', // We need to get this from GitHub
     REPO_NAME: 'dikuapplied.github.io',
-    OWNER: 'DIKUApplied'
+    OWNER: 'DIKUApplied',
+    PRIVATE_KEY: '' // We need to add the private key here
 };
